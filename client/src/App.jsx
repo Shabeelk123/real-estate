d0c81d7ed125f56673e8b93da45759c4bfa20016
Shabeelk123 <muhammedshabeelk@gmail.com>
2023-12-23 13:50:40 +0530

    create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import CreateListing  from "./pages/createListing";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Header from "./components/Header";
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
