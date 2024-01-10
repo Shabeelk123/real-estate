8cace7997284b15dae8b8af0e83f7a1e8c4decfe
Shabeelk123 <muhammedshabeelk@gmail.com>
2024-01-10 13:52:36 +0530

    created getUser listings API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
 import User from "../models/user.model.js";
+import List from "../models/listing.model.js";
 
 export const test = (req, res) => {
     res.json({
@@ -39,5 +40,19 @@ export const deleteUser = async(req, res, next) => {
         res.status(200).json('User has been deleted!');
     } catch (error) {
         next(error);
+    }
+};
+
+export const getUserListing = async(req, res, next) => {
+    //we are verifying user from cookie and request are same
+    if (req.user.id !== req.params.id) {
+        try {
+           const listing = await List.find({ userRef: req.params.id });
+            res.status(200).json(listing);
+        } catch (error) {
+            next(error);
+        }
+    } else{
+        return next(errorHandler(401,'you can only view your own listings!!'));
     }
 };
